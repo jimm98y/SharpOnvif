cbf013e8ef2a03dd851b613aefef110076cd8518
Lukas Volf <jimm98y@hotmail.com>
2024-05-12 14:24:18 +0200

    F Added Onvif discovery

--- src/SharpONVIF/SharpOnvifWCF/OnvifHelper.cs
+++ src/SharpONVIF/SharpOnvifWCF/OnvifHelper.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
 using System.ServiceModel.Dispatcher;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
+using System.Xml.XPath;
 
 namespace SharpOnvifWCF
 {
@@ -16,6 +24,8 @@ namespace SharpOnvifWCF
     /// </summary>
     public static class OnvifHelper
     {
+        #region WCF
+
         public static IEndpointBehavior CreateAuthenticationBehavior(string userName, string password)
         {
             return new PasswordDigestBehavior(userName, password);
@@ -41,6 +51,152 @@ namespace SharpOnvifWCF
         {
             return new EndpointAddress(endPointAddress);
         }
+
+        #endregion // WCF
+
+        #region Discovery
+
+        private class UdpState
+        {
+            public UdpClient Client { get; set; }
+            public IPEndPoint Endpoint { get; set; }
+            public IList<string> Result { get; set; }
+            public Action<string> Callback { get; set; }
+        }
+
+        public const int ONVIF_BROADCAST_TIMEOUT = 4000; // 4s timeout
+        public const int ONVIF_DISCOVERY_PORT = 3702;
+        public static string OnvifDiscoveryAddress = "239.255.255.250"; // only IPv4 networks are currently supported
+
+        private static readonly SemaphoreSlim _discoverySlim = new SemaphoreSlim(1);
+
+        /// <summary>
+        /// Discover ONVIF devices in the local network.
+        /// </summary>
+        /// <param name="ipAddress">IP address of the network interface to use (IP of the host computer).</param>
+        /// <param name="onDeviceDiscovered">Callback to be called when a new device is discovered.</param>
+        /// <param name="broadcastTimeout"><see cref="ONVIF_BROADCAST_TIMEOUT"/>.</param>
+        /// <param name="broadcastPort">Broadcast port - 0 to let the OS choose any free port.</param>
+        /// <returns>A list of discovered devices.</returns>
+        public static async Task<IList<string>> DiscoverAsync(string ipAddress, Action<string> onDeviceDiscovered = null, int broadcastTimeout = ONVIF_BROADCAST_TIMEOUT, int broadcastPort = 0)
+        {
+            if (ipAddress == null)
+                return new List<string>();
+
+            await _discoverySlim.WaitAsync();
+
+            const string WS_DISCOVERY_PROBE_MESSAGE =
+            "<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:a=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\">\r\n" +
+            "   <s:Header>\r\n" +
+            "      <a:Action s:mustUnderstand=\"1\">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</a:Action>\r\n" +
+            "      <a:MessageID>urn:uuid:e1245346-bee7-4ef0-82f2-c02a69b54d9c</a:MessageID>\r\n" + // uuid has to be replaced by a unique one before sending the request
+            "      <a:ReplyTo>\r\n" +
+            "        <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>\r\n" +
+            "      </a:ReplyTo>\r\n" +
+            "      <a:To>urn:schemas-xmlsoap-org:ws:2005:04:discovery</a:To>\r\n" +
+            "   </s:Header>\r\n" +
+            "   <s:Body>\r\n" +
+            "      <Probe xmlns=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\">\r\n" +
+            "         <d:Types xmlns:d=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\" xmlns:dp0=\"http://www.onvif.org/ver10/network/wsdl\">dp0:NetworkVideoTransmitter</d:Types>\r\n" +
+            "      </Probe>\r\n" +
+            "   </s:Body>\r\n" +
+            "</s:Envelope>\r\n";
+
+            IList<string> devices = new List<string>();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ipAddress), broadcastPort);
+            IPEndPoint multicastEndpoint = new IPEndPoint(IPAddress.Parse(OnvifDiscoveryAddress), ONVIF_DISCOVERY_PORT);
+
+            try
+            {
+                using (UdpClient client = new UdpClient(endPoint))
+                {
+                    UdpState s = new UdpState
+                    {
+                        Endpoint = endPoint,
+                        Client = client,
+                        Result = devices,
+                        Callback = onDeviceDiscovered
+                    };
+
+                    client.BeginReceive(DiscoveryMessageReceived, s);
+
+                    // Give the probe a unique urn:uuid (we must do this for each probe!)
+                    string uuid = Guid.NewGuid().ToString();
+                    string onvifDiscoveryProbe = WS_DISCOVERY_PROBE_MESSAGE.Replace("e1245346-bee7-4ef0-82f2-c02a69b54d9c", uuid.ToLowerInvariant());
+
+                    byte[] message = Encoding.UTF8.GetBytes(onvifDiscoveryProbe);
+                    await client.SendAsync(message, message.Count(), multicastEndpoint);
+
+                    // make sure we do not wait forever
+                    await Task.Delay(broadcastTimeout);
+
+                    return s.Result.OrderBy(x => x).ToArray();
+                }
+            }
+            finally
+            {
+                _discoverySlim.Release();
+            }
+        }
+
+        private static void DiscoveryMessageReceived(IAsyncResult result)
+        {
+            try
+            {
+                UdpClient client = ((UdpState)result.AsyncState).Client;
+                IPEndPoint endpoint = ((UdpState)result.AsyncState).Endpoint;
+                byte[] receiveBytes = client.EndReceive(result, ref endpoint);
+                string message = Encoding.UTF8.GetString(receiveBytes);
+                string host = endpoint.Address.ToString();
+                var devices = ((UdpState)result.AsyncState).Result;
+                var deviceEndpoint = ReadOnvifEndpoint(message);
+                if (deviceEndpoint != null)
+                {
+                    devices.Add(deviceEndpoint);
+
+                    var callback = ((UdpState)result.AsyncState).Callback;
+                    if(callback != null)
+                    {
+                        try
+                        {
+                            callback.Invoke(deviceEndpoint);
+                        }
+                        catch(Exception ex)
+                        {
+                            Debug.WriteLine(ex.Message);
+                        }
+                    }
+                }
+                client.BeginReceive(DiscoveryMessageReceived, result.AsyncState);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private static string ReadOnvifEndpoint(string message)
+        {
+            using (var textReader = new StringReader(message))
+            {
+                var document = new XPathDocument(textReader);
+                var navigator = document.CreateNavigator();
+
+                // local-name is used to ignore the namespace
+                var node = navigator.SelectSingleNode("//*[local-name()='XAddrs']/text()");
+                if (node != null)
+                {
+                    string[] addresses = node.Value.Split(' ');
+                    return addresses.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        #endregion // Discovery
     }
 
     /// <summary>
@@ -61,7 +217,6 @@ namespace SharpOnvifWCF
             _created = created.ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
             _password = password;
         }
-
 
         [XmlRoot(ElementName = "Password", Namespace = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd")]
         public class Password
