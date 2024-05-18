d3ae3ab3d37f824842f2987ef63992698f9471df
Lukas Volf <jimm98y@hotmail.com>
2024-05-18 23:10:02 +0200

    F Added sound event detection

--- src/SharpOnvifWCF/Client/SimpleOnvifClient.cs
+++ src/SharpOnvifWCF/Client/SimpleOnvifClient.cs
@@ -223,42 +223,67 @@ namespace SharpOnvifWCF.Client
 
         #region Common events
 
-        private static bool IsMotionEvent(string eventXml)
+        private static bool ContainsTrueValue(string eventXml)
         {
-            if (string.IsNullOrEmpty(eventXml))
-                return false;
-
-            return eventXml.ToLowerInvariant().Contains("RuleEngine/CellMotionDetector/Motion".ToLowerInvariant()) && eventXml.ToLowerInvariant().Contains("SimpleItem Name=\"IsMotion\"".ToLowerInvariant());
-        }
-
-        public static bool? IsMotionDetected(string eventXml)
-        {
-            if (!IsMotionEvent(eventXml)) return null;
-            return eventXml.ToLowerInvariant().Contains("SimpleItem Name=\"IsMotion\" Value=\"true\"".ToLowerInvariant());
+            string str = eventXml.ToLowerInvariant();
+            return str.Contains("value=\"true\"") || str.Contains("value=\"1\"");
         }
 
         public static bool? IsMotionDetected(NotificationMessageHolderType message)
         {
             return IsMotionDetected(string.Concat(message.Topic.Any.First().Value, message.Message.InnerXml));
         }
-
-        private static bool IsTamperEvent(string eventXml)
+        public static bool? IsMotionDetected(string eventXml)
+        {
+            if (!IsMotionEvent(eventXml)) return null;
+            return ContainsTrueValue(eventXml);
+        }
+        private static bool IsMotionEvent(string eventXml)
         {
             if (string.IsNullOrEmpty(eventXml))
                 return false;
 
-            return eventXml.ToLowerInvariant().Contains("RuleEngine/TamperDetector/Tamper".ToLowerInvariant()) && eventXml.ToLowerInvariant().Contains("SimpleItem Name=\"IsTamper\"".ToLowerInvariant());
-        }
-
-        public static bool? IsTamperDetected(string eventXml)
-        {
-            if (!IsTamperEvent(eventXml)) return null;
-            return eventXml.ToLowerInvariant().Contains("SimpleItem Name=\"IsTamper\" Value=\"true\"".ToLowerInvariant());
+            string str = eventXml.ToLowerInvariant();
+            return 
+                str.Contains("RuleEngine/CellMotionDetector/Motion".ToLowerInvariant()) ||
+                str.Contains("RuleEngine/MotionRegionDetector/Motion".ToLowerInvariant()) ||
+                str.Contains("VideoSource/MotionAlarm".ToLowerInvariant());
         }
 
         public static bool? IsTamperDetected(NotificationMessageHolderType message)
         {
             return IsTamperDetected(string.Concat(message.Topic.Any.First().Value, message.Message.InnerXml));
+        }
+        public static bool? IsTamperDetected(string eventXml)
+        {
+            if (!IsTamperEvent(eventXml)) return null;
+            return ContainsTrueValue(eventXml);
+        }
+        private static bool IsTamperEvent(string eventXml)
+        {
+            if (string.IsNullOrEmpty(eventXml))
+                return false;
+
+            string str = eventXml.ToLowerInvariant();
+            return str.Contains("RuleEngine/TamperDetector/Tamper".ToLowerInvariant());
+        }
+
+        public static bool? IsSoundDetected(NotificationMessageHolderType message)
+        {
+            return IsSoundDetected(string.Concat(message.Topic.Any.First().Value, message.Message.InnerXml));
+        }
+        public static bool? IsSoundDetected(string eventXml)
+        {
+            if (!IsSoundEvent(eventXml)) return null;
+            return ContainsTrueValue(eventXml);
+        }
+        private static bool IsSoundEvent(string eventXml)
+        {
+            if (string.IsNullOrEmpty(eventXml))
+                return false;
+
+            string str = eventXml.ToLowerInvariant();
+            return str.Contains("AudioAnalytics/Audio/DetectedSound".ToLowerInvariant());
         }
 
         #endregion // Common events
