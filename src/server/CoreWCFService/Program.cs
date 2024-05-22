9868dc10929e448811e00175e4e0b9e83811ddcc
Lukas Volf <jimm98y@hotmail.com>
2024-05-22 23:34:29 +0200

    F Added ASP.NET based authorization

--- src/server/CoreWCFService/Program.cs
+++ src/server/CoreWCFService/Program.cs
@@ -5,37 +5,20 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder();
+const string SCHEME_DIGEST = "Digest";
 
+var builder = WebApplication.CreateBuilder();
 builder.Services.AddServiceModelServices();
 builder.Services.AddServiceModelMetadata();
 builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
-
-const string SCHEME_DIGEST = "Digest";
-
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
 builder.Services
     .AddAuthentication(SCHEME_DIGEST)
     .AddScheme<AuthenticationSchemeOptions, DigestAuthenticationHandler>(SCHEME_DIGEST, null);
+builder.Services.AddAuthorization(); // this means we require Digest on all endpoints
 
 var app = builder.Build();
-
 app.UseAuthentication();
-app.Use(async (context, next) =>
-{
-    //if (context.Request.Path.StartsWithSegments("/Service.svc"))
-    //{
-        // Check if currently authenticated
-        var authResult = await context.AuthenticateAsync(SCHEME_DIGEST);
-        if (authResult.None)
-        {
-            // If the client hasn't authenticated, send a challenge to the client and complete request
-            await context.ChallengeAsync(SCHEME_DIGEST);
-            return;
-        }
-    //}
-    await next(context);
-});
 
 ((IApplicationBuilder)app).UseServiceModel(serviceBuilder =>
 {
