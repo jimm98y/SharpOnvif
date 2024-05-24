b76798f4c009f016775614513d8236782d74edee
Lukas Volf <jimm98y@hotmail.com>
2024-05-25 00:46:01 +0200

    F Improved discovery, added proper namespace parsing

--- src/SharpOnvifServer/Discovery/DiscoveryService.cs
+++ src/SharpOnvifServer/Discovery/DiscoveryService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -14,7 +15,7 @@ namespace SharpOnvifServer.Discovery
 {
     public class OnvifDiscoveryMessage
     {
-        public string Types { get; set; }
+        public List<Tuple<string, string>> Types { get; set; }
         public string MessageUuid { get; set; }
     }
 
@@ -29,10 +30,10 @@ namespace SharpOnvifServer.Discovery
             "onvif://www.onvif.org/Profile/T"
         };
 
-        public List<string> Types { get; set; } = new List<string>()
+        public List<Tuple<string, string>> Types { get; set; } = new List<Tuple<string, string>>
         {
-            "dn:NetworkVideoTransmitter",
-            "tds:Device"
+            new Tuple<string, string>("http://www.onvif.org/ver10/network/wsdl", "NetworkVideoTransmitter"),
+            new Tuple<string, string>("http://www.onvif.org/ver10/device/wsdl", "Device")
         };
 
         public string MAC { get; set; }
@@ -56,6 +57,8 @@ namespace SharpOnvifServer.Discovery
     /// </summary>
     public class DiscoveryService : IHostedService
     {
+        private static readonly Random _rnd = new Random();
+
         public const int ONVIF_DISCOVERY_PORT = 3702;
         public static string OnvifDiscoveryAddress = "239.255.255.250"; // only IPv4 networks are currently supported
 
@@ -91,10 +94,9 @@ namespace SharpOnvifServer.Discovery
 
                             string message = Encoding.UTF8.GetString(recvResult);
                             var parsedMessage = ReadOnvifEndpoint(message);
-                            if (parsedMessage != null) // TODO: valid types...
+                            if (parsedMessage != null && IsSearchingOurTypes(_options.Types, parsedMessage.Types)) 
                             {
-                                // reply
-                                string reply = CreateDiscoveryResponse(_options, parsedMessage.Types, parsedMessage.MessageUuid);
+                                string reply = CreateDiscoveryResponse(_options, parsedMessage.MessageUuid);
                                 var replyBytes = Encoding.UTF8.GetBytes(reply);
                                 int sentBytes = _udpClient.Client.SendTo(replyBytes, remoteEndpoint);
                             }
@@ -113,15 +115,42 @@ namespace SharpOnvifServer.Discovery
             return Task.CompletedTask;
         }
 
-        private static string CreateDiscoveryResponse(OnvifDiscoveryOptions options, string searchedType, string discoveryMessageUuid)
+        private bool IsSearchingOurTypes(List<Tuple<string, string>> types1, List<Tuple<string, string>> types2)
         {
-            // TODO: searchedType and namespaces
+            var types1combined = types1.Select(x => $"{x.Item1}#{x.Item2}").ToArray();
+            var types2combined = types2.Select(x => $"{x.Item1}#{x.Item2}").ToArray();
+            foreach(var type in types2combined)
+            {
+                if(types1combined.Contains(type))
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static string CreateDiscoveryResponse(OnvifDiscoveryOptions options, string discoveryMessageUuid)
+        {
+            Dictionary<string, string> nsPrefixes = new Dictionary<string, string>
+            {
+                { "http://www.w3.org/2003/05/soap-envelope", "env" },
+                { "http://schemas.xmlsoap.org/ws/2005/04/discovery", "d" },
+                { "http://schemas.xmlsoap.org/ws/2004/08/addressing", "wsadis" }
+            };
+
+            if (options.Types.Count > 0)
+            {
+                foreach(var type in options.Types) 
+                {
+                    nsPrefixes.Add(type.Item1, GetPrefix(nsPrefixes));
+                }
+            }
+
             string uuid = Guid.NewGuid().ToString().ToLowerInvariant();
             string message =
                 $"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" +
-                $"<env:Envelope xmlns:env=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:d=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\" " +
-                $"xmlns:wsadis=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" xmlns:dn=\"http://www.onvif.org/ver10/network/wsdl\" " +
-                $"xmlns:tds=\"http://www.onvif.org/ver10/device/wsdl\">" +
+                $"<env:Envelope " +
+                    BuildNamespaces(nsPrefixes) +
+                    ">" +
                     $"<env:Header>" +
                         $"<wsadis:MessageID>urn:uuid:{uuid}</wsadis:MessageID>\r\n" +
                         $"<wsadis:RelatesTo>{discoveryMessageUuid}</wsadis:RelatesTo>\r\n" +
@@ -134,11 +163,9 @@ namespace SharpOnvifServer.Discovery
                                 $"<wsadis:EndpointReference>" +
                                     $"<wsadis:Address>urn:uuid:{uuid}</wsadis:Address>\r\n" +
                                 $"</wsadis:EndpointReference>\r\n" +
-                                $"<d:Types>{BuildList(options.Types)}</d:Types>\r\n" +
-                                $"<d:Scopes>" +
-                                    BuildScopes(options) +                                  
-                                $"</d:Scopes>\r\n" +
-                                $"<d:XAddrs>{BuildList(options.ServiceAddresses)}</d:XAddrs>\r\n" +
+                                $"<d:Types>{BuildTypes(options.Types, nsPrefixes)}</d:Types>\r\n" +
+                                $"<d:Scopes>{BuildScopes(options)}</d:Scopes>\r\n" +
+                                $"<d:XAddrs>{BuildAddresses(options.ServiceAddresses)}</d:XAddrs>\r\n" +
                                 $"<d:MetadataVersion>10</d:MetadataVersion>\r\n" +
                             $"</d:ProbeMatch>\r\n" +
                         $"</d:ProbeMatches>\r\n" +
@@ -147,7 +174,44 @@ namespace SharpOnvifServer.Discovery
             return message;
         }
 
-        private static string BuildList(IEnumerable<string> values)
+        private static string BuildNamespaces(Dictionary<string, string> nsPrefixes)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach(var nsPrefix in nsPrefixes)
+            {
+                ret.Append($"xmlns:{nsPrefix.Value}=\"{nsPrefix.Key}\" ");
+            }
+            return ret.ToString();
+        }
+
+        private static string BuildTypes(List<Tuple<string, string>> values, Dictionary<string, string> nsPrefixes)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach (var type in values)
+            {
+                ret.Append($"{nsPrefixes[type.Item1]}:{type.Item2}");
+            }
+            return ret.ToString();
+        }
+
+        private static string GetPrefix(Dictionary<string, string> nsPrefixes)
+        {
+            string prefix = "";
+            const int minLen = 2;
+
+            while (true)
+            {
+                int num = _rnd.Next(0, 26); // Zero to 25
+                char c1 = (char)('a' + num);
+                prefix += c1;
+
+                if (prefix.Length >= minLen && !nsPrefixes.Values.Contains(prefix))
+                    break;
+            }
+            return prefix;
+        }
+
+        private static string BuildAddresses(IEnumerable<string> values)
         {
             return string.Join(' ', values);
         }
@@ -181,13 +245,18 @@ namespace SharpOnvifServer.Discovery
                 var document = new XPathDocument(textReader);
                 var navigator = document.CreateNavigator();
 
-                string types = "";
+                List<Tuple<string, string>> requestedTypes = new List<Tuple<string, string>>();
 
                 // local-name is used to ignore the namespace
                 var node = navigator.SelectSingleNode("//*[local-name()='Types']/text()");
                 if (node != null)
                 {
-                    types = node.Value;
+                    string[] parsedTypes = node.Value.Split(new char[] { ' ' });
+                    foreach(var type in parsedTypes)
+                    {
+                        string[] parsedTypeNs = type.Split(new char[] { ':' });
+                        requestedTypes.Add(new Tuple<string, string>(node.LookupNamespace(parsedTypeNs[0]), parsedTypeNs[1]));
+                    }
                 }
 
                 string uuid = "";
@@ -197,8 +266,8 @@ namespace SharpOnvifServer.Discovery
                     uuid = node.Value;
                 }
 
-                if (!string.IsNullOrEmpty(uuid) && !string.IsNullOrEmpty(types))
-                    return new OnvifDiscoveryMessage { Types = types, MessageUuid = uuid };
+                if (!string.IsNullOrEmpty(uuid) && requestedTypes.Count > 0)
+                    return new OnvifDiscoveryMessage { Types = requestedTypes, MessageUuid = uuid };
                 else
                     return null;
             }
