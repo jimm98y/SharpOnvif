e7adb3d825d38845265445cd7109f9639314e8dc
Lukas Volf <jimm98y@hotmail.com>
2025-08-27 22:03:51 +0200

    B Increased max message size to 1 MB - closes #35

--- src/SharpOnvifClient/OnvifBindingFactory.cs
+++ src/SharpOnvifClient/OnvifBindingFactory.cs
@@ -9,11 +9,21 @@ namespace SharpOnvifClient
     /// </summary>
     public static class OnvifBindingFactory
     {
+        /// <summary>
+        /// The default message size is 65536 which can be too small nowdays - increase it to 1 MB.
+        /// </summary>
+        private const int MAX_MESSAGE_SIZE = 1048576;
+
+        /// <summary>
+        /// Create an Onvif WCF binding for the Digest authentication using Soap 1.2.
+        /// </summary>
         public static Binding CreateBinding()
         {
             var httpTransportBinding = new HttpTransportBindingElement
             {
-                AuthenticationScheme = AuthenticationSchemes.Digest
+                AuthenticationScheme = AuthenticationSchemes.Digest,
+                MaxReceivedMessageSize = MAX_MESSAGE_SIZE,
+                MaxBufferSize = MAX_MESSAGE_SIZE
             };
             var textMessageEncodingBinding = new TextMessageEncodingBindingElement
             {
--- src/SharpOnvifServer/OnvifBindingFactory.cs
+++ src/SharpOnvifServer/OnvifBindingFactory.cs
@@ -10,6 +10,11 @@ namespace SharpOnvifServer
     public static class OnvifBindingFactory
     {
         /// <summary>
+        /// The default message size is 65536 which can be too small nowdays - increase it to 1 MB.
+        /// </summary>
+        private const int MAX_MESSAGE_SIZE = 1048576;
+
+        /// <summary>
         /// Create an Onvif CoreWCF binding for the Digest authentication using Soap 1.2.
         /// </summary>
         /// <returns><see cref="CustomBinding"/> using Digest authentication and Soap 1.2.</returns>
@@ -18,6 +23,8 @@ namespace SharpOnvifServer
             var httpTransportBinding = new HttpTransportBindingElement
             {
                 AuthenticationScheme = AuthenticationSchemes.Digest,
+                MaxReceivedMessageSize = MAX_MESSAGE_SIZE,
+                MaxBufferSize = MAX_MESSAGE_SIZE
             };
             var textMessageEncodingBinding = new TextMessageEncodingBindingElement
             {
